413cef397032524f3ff90d276a82bf09c39fe00b
olivia mo <olivia.mo@bigtincan.com>
2019-11-16 12:14:55 +1100

    Add app and search bar to page

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  state = { term: '' };
+
+  handleInputChange = event => {
+    this.setState({ term: event.target.value });
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.handleFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.handleInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
