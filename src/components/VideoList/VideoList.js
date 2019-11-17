d3c1f74cc7d93e32ddea46a8e621f423598b565d
olivia mo <olivia.mo@bigtincan.com>
2019-11-17 22:44:28 +1100

    format layout and add default videos

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -7,7 +7,7 @@ const VideoList = props => {
     onVideoSelect
   } = props;
 
-  const renderedList = videos.map(video => <VideoItem video={video} onVideoSelect={onVideoSelect} />);
+  const renderedList = videos.map(video => <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />);
 
   return (
     <div className="ui relaxed divided list">{renderedList}</div>
